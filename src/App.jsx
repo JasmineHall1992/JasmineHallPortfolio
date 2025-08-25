e7131dc23da77035610699d88a1b22416eee5d7d
JasmineHall1992 <jasminehall5935@gmail.com>
2025-08-25 12:01:31 -0500

    finished homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import ResumePage from './components/ResumePage';
 import SocialPage from './components/SocialPage';
 import Navigation from './components/Navigation';
 
+// Create a component that uses useLocation inside the Router
+function AppContent() {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+
+  return (
+    <div className="App">
+      {/* Only show Navigation if NOT on homepage */}
+      {!isHomePage && <Navigation />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/resume" element={<ResumePage />} />
+        <Route path="/social" element={<SocialPage />} />
+      </Routes>
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/resume" element={<ResumePage />} />
-          <Route path="/social" element={<SocialPage />} />
-        </Routes>
-      </div>
+      <AppContent />
     </Router>
   );
 }
